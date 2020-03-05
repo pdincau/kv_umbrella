aa01106abe77006374fb01318fae9566c0cb54ca
Paolo D'Incau <paolo.dincau@gmail.com>
2020-03-05 18:44:45 +0100

    fix bug on tcp reading part

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -19,8 +19,8 @@ defmodule KVServer.Command do
     iex> KVServer.Command.parse "DELETE shopping eggs\r\n"
     {:ok, {:delete, "shopping", "eggs"}}
 
-Unknown commands or commands with the wrong number of
-arguments return an error:
+    Unknown commands or commands with the wrong number of
+    arguments return an error:
 
     iex> KVServer.Command.parse "UNKNOWN shopping eggs\r\n"
     {:error, :unknown_command}
@@ -30,6 +30,7 @@ arguments return an error:
     """
 
     def parse(line) do
+      IO.inspect line
       case String.split(line) do
         ["CREATE", bucket] -> {:ok, {:create, bucket}}
         ["GET", bucket, key] -> {:ok, {:get, bucket, key}}
@@ -39,5 +40,42 @@ arguments return an error:
       end
     end
 
+    @doc """
+    Runs the given command
+    """
+
+    def run(command)
+
+    def run({:create, bucket}) do
+      KV.Registry.create(KV.Registry, bucket)
+      {:ok, "OK\r\n"}
+    end
+
+    def run({:get, bucket, key}) do
+      lookup(bucket, fn pid ->
+        value = KV.Bucket.get(pid, key)
+        {:ok, "#{value}\r\nOK\r\n"}
+      end)
+    end
     
+    def run({:put, bucket, key, value}) do
+      lookup(bucket, fn pid ->
+        KV.Bucket.put(pid, key, value)
+        {:ok, "OK\r\n"}
+      end)
+    end
+    
+    def run({:delete, bucket, key}) do
+      lookup(bucket, fn pid ->
+        KV.Bucket.delete(pid, key)
+        {:ok, "OK\r\n"}
+      end)
+    end
+    
+    defp lookup(bucket, callback) do
+      case KV.Registry.lookup(KV.Registry, bucket) do
+        {:ok, pid} -> callback.(pid)
+        :error -> {:error, :not_found}
+      end
+    end
 end
