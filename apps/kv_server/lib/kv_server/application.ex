956da9ba6170e605d148c9b69e117140fb4d3d8a
Paolo D'Incau <paolo.dincau@gmail.com>
2020-03-05 16:30:46 +0100

    acceptor is permament

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -9,7 +9,7 @@ defmodule KVServer.Application do
     port = String.to_integer(System.get_env("PORT") || "4040")
     children = [
       {Task.Supervisor, name: KVServer.TaskSupervisor},
-      {Task, fn -> KVServer.accept(port) end}
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
     ]
 
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
